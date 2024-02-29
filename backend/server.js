39e22f0d36cd9f2bab2fef7670ba9aa322399295
tochi27 <tochukwuamaechina2703@gmail.com>
2024-02-29 11:47:55 +0100

    Cors again

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,8 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({
-  origin: ["http://localhost:3000/", "https://mern-task-app-3h2x.onrender.com"]
-}));
+app.use(cors());
 app.use("/api/tasks", taskRoutes);
-
-
 
 mongoose
   .connect(process.env.MONGO_URI)
